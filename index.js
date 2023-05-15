1204bc8bfea5233554e770755de588e0f46b89e3
Tomasz Zawadzki <103272474+ExpertoMontanero@users.noreply.github.com>
2023-05-15 21:31:59 +0200

    Add files via upload
    end for today :>

--- index.js
+++ index.js
@@ -10,28 +10,30 @@ $(".form-control").on("focus", function () {
     $(this).addClass("hover");
 });
 
-var chAlert=true;
-var cnAlert=true;
+var chAlert = true;
+var cnAlert = true;
 $("#confirm-btn").on("click", function () {
     // CHECKING INPUTS
 
     //cardholder name
-    if (!$("#cardholder-name-input").val()&&chAlert==true) {
+    if (!$("#cardholder-name-input").val() && chAlert == true) {
         $(".cardholder-form").append("<p class='alert' id='cardholder-alert'>Please enter proper name</p>");
-        chAlert=false;
+        chAlert = false;
     }
-    if($("#cardholder-name-input").val()) {
-        chAlert=true;
+    else if ($("#cardholder-name-input").val()) {
+        chAlert = true;
         $("#cardholder-alert").remove();
     }
 
     //card number
-    if (!$("#card-number-input").val()&&cnAlert==true) {
+
+    if (!$.isNumeric($("#card-number-input").val()) && cnAlert == true) {
         $(".card-number-form").append("<p class='alert' id='card-number-alert'>Wrong format, 16 numbers only</p>");
-        cnAlert=false;
+        cnAlert = false;
     }
-    if($("#card-number-input").val()) {
-        cnAlert=true;
+    else if ($.isNumeric($("#card-number-input").val())) {
+        cnAlert = true;
         $("#card-number-alert").remove();
     }
 });
+
