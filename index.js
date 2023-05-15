31222ea9e497892c2183976cf45ca3182c199488
Tomasz Zawadzki <103272474+ExpertoMontanero@users.noreply.github.com>
2023-05-15 16:16:08 +0200

    Add files via upload
    input hover states checking

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+$(".form-control").hover(
+    function () {
+        $(this).addClass("hover");
+    }, function () {
+        $(this).removeClass("hover");
+    }
+);
 
+$(".form-control").on("focus", function () {
+    $(this).addClass("hover");
+});
