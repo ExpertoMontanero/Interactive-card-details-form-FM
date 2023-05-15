a67da4c5d3e28157778330b55e95bfc0202a18c1
Tomasz Zawadzki <103272474+ExpertoMontanero@users.noreply.github.com>
2023-05-15 19:14:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,3 +9,29 @@ $(".form-control").hover(
 $(".form-control").on("focus", function () {
     $(this).addClass("hover");
 });
+
+var chAlert=true;
+var cnAlert=true;
+$("#confirm-btn").on("click", function () {
+    // CHECKING INPUTS
+
+    //cardholder name
+    if (!$("#cardholder-name-input").val()&&chAlert==true) {
+        $(".cardholder-form").append("<p class='alert' id='cardholder-alert'>Please enter proper name</p>");
+        chAlert=false;
+    }
+    if($("#cardholder-name-input").val()) {
+        chAlert=true;
+        $("#cardholder-alert").remove();
+    }
+
+    //card number
+    if (!$("#card-number-input").val()&&cnAlert==true) {
+        $(".card-number-form").append("<p class='alert' id='card-number-alert'>Wrong format, 16 numbers only</p>");
+        cnAlert=false;
+    }
+    if($("#card-number-input").val()) {
+        cnAlert=true;
+        $("#card-number-alert").remove();
+    }
+});
